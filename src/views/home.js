8768e8b2257d2399b07356f23f51a1849edb7ecb
anthonyhxx <anthonyhaugabrook56@gmail.com>
2024-08-29 16:11:50 -0400

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,212 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container1">
+      <Helmet>
+        <title>Dependent Last Dinosaur</title>
+        <meta property="og:title" content="Dependent Last Dinosaur" />
+      </Helmet>
+      <div className="home-container2">
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link10"
+        >
+          tops
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link11"
+        >
+          bottoms
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link12"
+        >
+          outerwear
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link13"
+        >
+          accessories
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link14"
+        >
+          shoes
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link15"
+        >
+          ammunition
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link16"
+        >
+          apparel
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link17"
+        >
+          art
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link18"
+        >
+          automotive
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link19"
+        >
+          basketball
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link20"
+        >
+          books
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link21"
+        >
+          collectables
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link22"
+        >
+          designer
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link23"
+        >
+          drugs
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link24"
+        >
+          guns
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link25"
+        >
+          home &amp; garden
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link26"
+        >
+          jewelry
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link27"
+        >
+          knives
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link28"
+        >
+          medical supplies
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="home-link29"
+        >
+          packaging
+        </a>
+      </div>
+      <div className="home-container3">
+        <div className="home-container4"></div>
+        <img
+          alt="image"
+          src="/6below-recoveredwhite-200h.png"
+          className="home-image"
+        />
+        <div className="home-container5">
+          <a href="https://6below.net" className="home-link30">
+            SIXBELOW
+          </a>
+          <a
+            href="mailto:sixbelow.world@gmail.com?subject="
+            className="home-link31"
+          >
+            contact
+          </a>
+          <a
+            href="https://instagram.com/user__6below"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="home-link32"
+          >
+            socials
+          </a>
+          <input
+            type="text"
+            placeholder="search..."
+            className="home-textinput input"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
